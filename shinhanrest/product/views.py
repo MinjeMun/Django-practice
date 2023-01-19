35d9f97c7310bcc25fcf2a70e4959c38be0d8eb4
MinjeMun <mmj010220@gmail.com>
2023-01-19 11:21:44 +0900

    feat: Update ProductDetailView CRUD
    CRUD 기능 생성

--- shinhanrest/product/views.py
+++ shinhanrest/product/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, mixins
 from .models import Product
 from .serializers import ProductSerializer
+from.paginations import ProductLargePagination
 
 # 상속을 여러개 받음 (부모-자식이랑은 좀 다른 관계)
 
@@ -8,8 +9,10 @@ class ProductListView(
     mixins.ListModelMixin, 
     mixins.CreateModelMixin,     
     generics.GenericAPIView
-    ): 
+): 
     serializer_class = ProductSerializer
+    pagination_class = ProductLargePagination
+
 
     def get_queryset(self):
         products = Product.objects.all()
@@ -29,3 +32,23 @@ class ProductListView(
 
     def post(self, request, *args, **kwargs):
         return self.create(request, args, kwargs)
+
+class ProductDetailView(
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    generics.GenericAPIView
+):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, args, kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, args, kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.partial_update(request, args, kwargs)
